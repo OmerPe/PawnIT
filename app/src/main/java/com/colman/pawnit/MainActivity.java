857683abb58ca60cb54085f88bdff147f0ff3da0
Omer <omer.pe4@gmail.com>
2021-06-18 12:51:52 +0300

    added activity files and menu logic

--- app/src/main/java/com/colman/pawnit/MainActivity.java
+++ app/src/main/java/com/colman/pawnit/MainActivity.java
@@ -1,23 +1,27 @@
 package com.colman.pawnit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView leftDrawer;
     NavigationView rightDrawer;
     ImageButton menuBtn;
     ImageButton profileBtn;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +35,11 @@ public class MainActivity extends AppCompatActivity {
         menuBtn = findViewById(R.id.MainAct_MenuBtn);
         profileBtn = findViewById(R.id.MainAct_UserIcon);
 
+
         /*------------------- Nav drawer menu ---------------------------- */
         //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,menuBtn,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        leftDrawer.bringToFront();
+        rightDrawer.bringToFront();
         menuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +53,16 @@ public class MainActivity extends AppCompatActivity {
                 drawerLayout.openDrawer(Gravity.RIGHT);
             }
         });
+
+        leftDrawer.setNavigationItemSelectedListener(this);
+        rightDrawer.setNavigationItemSelectedListener(this);
+
+        leftDrawer.setCheckedItem(R.id.MainMenu_home);
+
+        // hide items
+        logout();
+
+
     }
 
     @Override
@@ -57,5 +74,85 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.MainMenu_home:
+                break;
+            case R.id.MainMenu_2ndHand:
+                startActivity(new Intent(MainActivity.this,secondHandActivity.class));
+                break;
+            case R.id.MainMenu_auction:
+                startActivity(new Intent(MainActivity.this,auctionActivity.class));
+                break;
+            case R.id.MainMenu_pawn:
+                startActivity(new Intent(MainActivity.this,pawnActivity.class));
+                break;
+            case R.id.my_listings:
+                startActivity(new Intent(MainActivity.this,myListingsActivity.class));
+                break;
+            case R.id.my_pawns:
+                startActivity(new Intent(MainActivity.this,myPawnsActivity.class));
+                break;
+            case R.id.history:
+                startActivity(new Intent(MainActivity.this,userHistoryActivity.class));
+                break;
+            case R.id.contacts:
+                startActivity(new Intent(MainActivity.this,contactsActivity.class));
+                break;
+            case R.id.user_settings:
+                startActivity(new Intent(MainActivity.this,userSettingsActivity.class));
+                break;
+            case R.id.app_settings:
+                startActivity(new Intent(MainActivity.this,appSettingsActivity.class));
+                break;
+            case R.id.MainMenu_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.MainMenu_rate:
+                Toast.makeText(this, "Rate", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                logout();
+                Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.login:
+                login();
+                Toast.makeText(this, "Logged In", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else if (this.drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            this.drawerLayout.closeDrawer(GravityCompat.END);
+        }
+
+        return true;
+    }
+
+    private void logout(){
+        Menu profileMenu = rightDrawer.getMenu();
+        profileMenu.findItem(R.id.logout).setVisible(false);
+        profileMenu.findItem(R.id.contacts).setVisible(false);
+        profileMenu.findItem(R.id.my_listings).setVisible(false);
+        profileMenu.findItem(R.id.my_pawns).setVisible(false);
+        profileMenu.findItem(R.id.history).setVisible(false);
+        profileMenu.findItem(R.id.user_settings).setVisible(false);
+        profileMenu.findItem(R.id.login).setVisible(true);
+    }
+
+    private void login(){
+        Menu profileMenu = rightDrawer.getMenu();
+        profileMenu.findItem(R.id.logout).setVisible(true);
+        profileMenu.findItem(R.id.contacts).setVisible(true);
+        profileMenu.findItem(R.id.my_listings).setVisible(true);
+        profileMenu.findItem(R.id.my_pawns).setVisible(true);
+        profileMenu.findItem(R.id.history).setVisible(true);
+        profileMenu.findItem(R.id.user_settings).setVisible(true);
+        profileMenu.findItem(R.id.login).setVisible(false);
     }
 }
---
+++ app/src/main/java/com/colman/pawnit/auctionActivity.java
@@ -0,0 +1,14 @@
+package com.colman.pawnit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class auctionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auction);
+    }
+}
