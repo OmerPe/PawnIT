b410610a877ae66d298ab7b56f5850ed99a66443
Daniel Zaharan <sinfff@cs.colman.ac.il>
2021-06-29 16:01:35 +0300

    created a testing activity to check if it works and created view model
    note to myself need to nmake type converters as Room does not support them properly
    
    no need to pull this branch yet just saving information since im making changes in the code

---
+++ app/src/main/java/com/colman/pawnit/NoteActivity.java
@@ -0,0 +1,31 @@
+package com.colman.pawnit;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class NoteActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+               //testing later.
+                Toast.makeText(NoteActivity.this,"testing stuff",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
