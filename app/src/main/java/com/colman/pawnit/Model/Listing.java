653bc00d2fe1d70f1b2dbc875ed5578ab613014f
DorPop <dorp273@gmail.com>
2021-07-03 15:18:44 +0300

    test for reading from the database

--- app/src/main/java/com/colman/pawnit/Model/Listing.java
+++ app/src/main/java/com/colman/pawnit/Model/Listing.java
@@ -13,5 +13,69 @@ public class Listing {
     private Date date;
     private List<String> images;
 
+    public String getListingID() {
+        return listingID;
+    }
 
+    public void setListingID(String listingID) {
+        this.listingID = listingID;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    
 }
--- app/src/main/java/com/colman/pawnit/pawnActivity.java
+++ app/src/main/java/com/colman/pawnit/pawnActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +13,17 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class pawnActivity extends AppCompatActivity {
 
     private String itemName, desc;
     private int img;
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -30,22 +38,26 @@ public class pawnActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
 
 
-
     }
 
     class MyAdapter extends BaseAdapter {
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference mDatabaseRef = mDatabase.getReference("Users");
+
         @Override
         public int getCount() {
-            return 10;
+            DataSnapshot ds = mDatabaseRef.child("Users").get().getResult();
+            return 3;
         }
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            LayoutInflater inflater = getLayoutInflater();
-            View v = inflater.inflate(R.layout.item_list_row,null);
 
-            TextView nameTv=v.findViewById(R.id.listrow_title_tv);
-            nameTv.setText("Item" + i);
+            LayoutInflater inflater = getLayoutInflater();
+            View v = inflater.inflate(R.layout.item_list_row, null);
+
+            TextView nameTv = v.findViewById(R.id.listrow_title_tv);
+            nameTv.setText(mDatabaseRef.child("Users").child("userName").getKey());
 
             return v;
         }
