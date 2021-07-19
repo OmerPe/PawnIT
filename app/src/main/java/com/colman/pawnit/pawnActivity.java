df32da1624b9008a4d7de14a10985853f5ec065b
DorPop <dorp273@gmail.com>
2021-07-19 17:03:19 +0300

    added pawn fragment

--- app/src/main/java/com/colman/pawnit/pawnActivity.java
+++ app/src/main/java/com/colman/pawnit/pawnActivity.java
@@ -1,29 +1,16 @@
 package com.colman.pawnit;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-
-import com.colman.pawnit.Model.Listing;
-import com.colman.pawnit.Model.Model;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.List;
+import android.widget.ProgressBar;
 
 public class pawnActivity extends AppCompatActivity {
-
-    List<Listing> data;
-
+    static ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,21 +18,7 @@ public class pawnActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pawn);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
-        RecyclerView rView = findViewById(R.id.pawn_activity_list);
-        rView.setHasFixedSize(true);
-
-        RecyclerView.LayoutManager manager = new LinearLayoutManager(this);
-        rView.setLayoutManager(manager);
-
-//        data = Model.instance.getListingData();
-//        data.add(new Listing());
-//        data.add(new Listing());
-//        data.add(new Listing());
-//        data.add(new Listing());
-
-        MyAdapter adapter = new MyAdapter();
-        rView.setAdapter(adapter);
-
+        progressBar = findViewById(R.id.pawn_progress_bar);
     }
 
     static class MyViewHolder extends RecyclerView.ViewHolder {
@@ -59,14 +32,13 @@ public class pawnActivity extends AppCompatActivity {
 
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = getLayoutInflater().inflate(R.layout.pawn_list_row, parent, false);
+            View view = getLayoutInflater().inflate(R.layout.fragment_pawn, parent, false);
             MyViewHolder holder = new pawnActivity.MyViewHolder(view);
             return holder;
         }
 
         @Override
         public void onBindViewHolder(MyViewHolder holder, int position) {
-            Listing listing = data.get(position);
 
         }
 
